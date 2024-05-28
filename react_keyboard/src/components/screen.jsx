699fc3b2d8ba5ae754bdb6cf0212afde728e30d5
Avital Kahana <avitalka@kan.org.il>
2024-05-28 10:17:30 +0300

    work enter

--- react_keyboard/src/components/screen.jsx
+++ react_keyboard/src/components/screen.jsx
@@ -4,13 +4,22 @@ import "./screen.css";
 export default function Screen({ styledText, myDir }) {
     return (
         <div dir={myDir} className="screen">
-            {styledText.map((part, index) => (              
-                <span
-                    key={index}
-                    style={{ color: part.style.color, fontFamily: part.style.font , fontSize: part.style.fontSize}}
-                >
-                    {part.text}
-                </span>
+            {styledText.map((part, index) => (
+                <React.Fragment key={index}>
+                    {part.text === '\n' ? (
+                        <br />
+                    ) : (
+                        <span
+                            style={{
+                                color: part.style.color,
+                                fontFamily: part.style.font,
+                                fontSize: part.style.fontSize
+                            }}
+                        >
+                            {part.text}
+                        </span>
+                    )}
+                </React.Fragment>
             ))}
         </div>
     );
